5a3963c6f73f2698437d55280ddcae42a0386fa6
Maicon Mares <maiconmaresunb@gmail.com>
2020-06-12 22:05:29 -0300

    Implementing search.

--- src/js/searchMovies.js
+++ src/js/searchMovies.js
@@ -1,29 +1,23 @@
-import { apiKey } from './renderMovies.js';
-
-let searchBoxMain = document.getElementById('search-box-main');
-let targetButton = document.querySelector('button#button-search-main');
+import { apiKey, modifyImgUrl } from './renderMovies.js';
+import * as elementHandler from './elementsHandler.js';
 
 let resultRequest = {};
 
-async function makeSearch(event) {
-    event.preventDefault();
-
-    let termSearched = searchBoxMain.value;
-    let searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&page=1&include_adult=false&query=${termSearched}`;
-    
-    window.location = './moviesResult.html';
-    
+async function makeSearch() {
+    let movieName = localStorage.getItem('termSearched');
+    let searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&page=1&include_adult=false&query=${movieName}`;
+        
     try {
         const response = await fetch(searchUrl);
         if (response.ok) {
             const responseJson = await response.json();
             resultRequest = responseJson;
-            console.log(responseJson);
+
+            modifyImgUrl(resultRequest.results);
         }
     } catch (error) {
         console.log(error.message);
     }
-    
 }
 
-targetButton.addEventListener("click", makeSearch);
+makeSearch();
