6e9b92dbbaff2bee16fc62bcd7b4379b4beb4193
Hérya Rodrigues <rodriguerya@gmail.com>
2022-02-05 19:54:14 -0300

    grafo direcionado

--- src/js/graphStructure.js
+++ src/js/graphStructure.js
@@ -39,5 +39,43 @@ class Graph {
         }
     }
 
+    printGraph() {
+        var get_keys = this.AdjList.keys();
+        for (var i of get_keys){
+            var get_values = this.AdjList.get(i);
+            var conc = "";     
+
+            for (var j in get_values){
+                conc += j + " ";
+            }
+            console.log(i + " -> " + conc);
+        }
+
+    }
 
 }
+
+//// exemplos teste
+
+var g = new Graph(12);
+
+var nodes = [ 'silicon valley', 'jogos vorazes', 'gravidade', 'apollo 14', 'duro de matar', 'o regresso', 'user1', 'user2', 'user3', 'user4', 'user5', 'user6' ];
+
+for (var i = 0; i < nodes.length; i++) {
+    g.addNode(nodes[i]);
+}
+
+g.addEdge('silicon valley', 'user4');
+g.addEdge('silicon valley', 'user1');
+g.addEdge('jogos vorazes', 'user2');
+g.addEdge('gravidade', 'user6');
+g.addEdge('gravidade', 'user5');
+g.addEdge('apollo 14', 'user6');
+g.addEdge('duro de matar', 'user5');
+g.addEdge('duro de matar', 'user3');
+g.addEdge('o regresso', 'user5');
+g.addEdge('o regresso', 'user3');
+
+
+g.printGraph();
+
