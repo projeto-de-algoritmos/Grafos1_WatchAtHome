605b3efc6e456e2e8309becb3b90e553a494ca26
Hérya Rodrigues <rodriguerya@gmail.com>
2022-02-05 11:11:53 -0300

    Estrutura inicial do grafo

---
+++ src/js/graphStructure.js
@@ -0,0 +1,41 @@
+class Graph {
+    constructor(numNodes){
+        this.numNodes = numNodes;
+        this.AdjList =  new Map();
+    }
+
+    addNode(v){
+        this.AdjList.set(v, []);
+    }
+
+
+    addEdge(v, u){
+        this.AdjList.get(v).push(w);
+        this.AdjList.get(w).push(v);
+    }
+
+    BFS(initialNode){
+
+        var visited =  {};
+        var q = new Queue();
+        visited[initialNode] = true;
+        q.enqueue(initialNode);
+
+        while(!q.isEmpty()){
+            var getQueueElement = q.dequeue();
+            console.log(getQueueElement);
+            var getList = this.AdjList.get(getQueueElement);
+    
+            for (var i in getList) {
+                var n = getList[i];
+    
+                if (!visited[n]) {
+                    visited[n] = true;
+                    q.enqueue(n);
+                }
+            }
+        }
+    }
+
+
+}
