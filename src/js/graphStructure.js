16868cf7b6882934ced2c40f73533c4ef264baa7
Hérya Rodrigues <rodriguerya@gmail.com>
2022-02-05 19:04:55 -0300

    Correções

--- src/js/graphStructure.js
+++ src/js/graphStructure.js
@@ -1,4 +1,4 @@
-import Queue from "./queue";
+import Queue from "./queue.js";
 
 
 class Graph {
@@ -14,7 +14,6 @@ class Graph {
 
     addEdge(v, u){
         this.AdjList.get(v).push(w);
-        this.AdjList.get(w).push(v);
     }
 
     BFS(initialNode){
@@ -42,50 +41,3 @@ class Graph {
 
 
 }
-
-///////////////////////// 
-// bipartido
-//class Graph {
-//    constructor(numNodes){
-//        this.numNodes = numNodes;
-//        this.AdjListMovie =  new Map();
-//        this.AdjListUser =  new Map();
-//    }
-//
-//    addNodeMovie(v){
-//        this.AdjListMovie.set(v, []);
-//    }
-//
-//    addNodeUser(v){
-//        this.AdjListUser.set(v, []);
-//    }
-//
-//    addEdge(movie, user){
-//        this.AdjListUser.get(user).push(movie);
-//        this.AdjListMovie.get(movie).push(user);
-//    }
-//
-//    BFS(initialNode){
-//
-//        var visited =  {};
-//        var q = new Queue();
-//        visited[initialNode] = true;
-//        q.enqueue(initialNode);
-//
-//        while(!q.isEmpty()){
-//            var getQueueElement = q.dequeue();
-//            console.log(getQueueElement);
-//            var getList = this.AdjList.get(getQueueElement);
-//    
-//            for (var i in getList) {
-//                var n = getList[i];
-//    
-//                if (!visited[n]) {
-//                    visited[n] = true;
-//                    q.enqueue(n);
-//                }
-//            }
-//        }
-//    }
-//
-//}
