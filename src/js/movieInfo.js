c483411255efbb1dba9b5596fbf165d526e22ceb
MaiconMares <maiconmaresunb@gmail.com>
2022-02-05 21:46:30 -0300

    feat(src): Adding favorite button and conditionally rendering it.

--- src/js/movieInfo.js
+++ src/js/movieInfo.js
@@ -1,10 +1,12 @@
+let movieInfo = {};
+
 async function renderMovieInfo() {
     let movie = await JSON.parse(localStorage.getItem('movieInfo'));
 
     let newUrl = `https://www.youtube.com/embed/${await movie.trailer_url}`;
     movie = {...movie, trailer_url: newUrl};
 
-    let movieInfo = {
+    movieInfo = {
         movie: [movie]
     }
 
@@ -14,6 +16,27 @@ async function renderMovieInfo() {
     const compiledHTML = template(movieInfo);
     const target = document.getElementsByClassName('movie-info')[0];
     target.innerHTML = compiledHTML;
+    favoriteBtnHandler();
+}
+
+function favoriteBtnHandler() {
+    let favBtn = document.querySelector('div#favorite-btn');
+    let isLogged = JSON.parse(localStorage.getItem('isLogged'));
+
+    if (!isLogged)
+    favBtn.classList.add('disable-container');
+
+    favBtn.addEventListener('click', addMovieToGraph);
+}
+
+function addMovieToGraph(event) {
+    event.preventDefault();
+
+    //Adicionar implementação da adição do nó ao grafo
+    //Criar lista com os filmes já adicionados ao grafo e armazenar isso como objeto
+    //Verificar se, antes de adicionar, o filme já não está no grafo
+    let {movie: [{title}]} = movieInfo;
+    
 }
 
 renderMovieInfo();
